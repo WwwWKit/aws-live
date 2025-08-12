5c5632a9846120e9e76d212b6ce6f297ba2fd6f6
WAI KIT <117494000+WwwWKit@users.noreply.github.com>
2025-08-12 19:45:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cs7cgtdw6vtf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "loowaikit-employee"
+customregion = "us-east-1"
+
